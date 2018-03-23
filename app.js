24115dd327fca69d28b95871bccd16ed96c5d1fd
Kelly Moore <kellym@waffles.local>
2018-03-23 14:32:00 +0000

    Inital commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const restify = require('restify');
+const builder = require('botbuilder');
+
+require('dotenv').config()
+
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978,
+    function () {
+        console.log('%s listening to %s',server.name,server.url);
+    }
+);
+
+const connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+server.post('/api/messages', connector.listen());
+
+const bot = new builder.UniversalBot(connector);
+
+bot.dialog('/',function (session){
+    session.send("Hello World")
+});
