2157ff8ac0f8b66c510bd42cec618e6e5ada488f
Mary-Jane McBride <maryjanem@hotmail.co.uk>
2018-03-30 16:00:54 +0100

    Basic skeleton for chatbot definition & placeholder images

--- app.js
+++ app.js
@@ -22,3 +22,8 @@ const bot = new builder.UniversalBot(connector);
 bot.dialog('/',function (session){
     session.send("Hello World!")
 });
+
+bot.dialog('/chatbot-definition', require('./dialogs/chatbot-definition')).triggerAction({ matches:[/^define/i] });
+bot.dialog('/chatbot-example-alexa', require('./dialogs/chatbot-example-alexa')).triggerAction({ matches:[/^I think I need an example/i] });
+bot.dialog('/chatbot-example-spotify', require('./dialogs/chatbot-example-spotify')).triggerAction({ matches:[/^Give me another/i] });
+bot.dialog('/chatbot-website-differences', require('./dialogs/chatbot-website-differences')).triggerAction({ matches:[/^Why would I choose a chatbot over a website or app?/i] });
