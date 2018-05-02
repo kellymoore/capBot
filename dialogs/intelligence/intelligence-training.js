bec9167b7f67b8768ff75bf77131b7bcf412eb57
Mary-Jane McBride <maryjanem@hotmail.co.uk>
2018-05-02 09:32:19 +0100

    Changing references to plural 'we' and 'yourselves' to be singular (tragic)

--- dialogs/intelligence/intelligence-training.js
+++ dialogs/intelligence/intelligence-training.js
@@ -6,12 +6,12 @@ function trainingExplained(session){
     var card = new builder.HeroCard(session)
     .title("CAPBOT: The Origins Story")
     .subtitle("Way back when I was just a baby bot...")
-    .text("Kelly & MJ spoke to me and asked me lots of questions until I understood what they were saying.")
+    .text("MJ spoke to me and asked me lots of questions until I understood what she was saying.")
     .images([
         builder.CardImage.create(session, "https://i.imgur.com/z6ifzjv.png")
     ])
     .buttons([
-        builder.CardAction.imBack(session, "Do you have a diagram?", "Kelly & MJ can tell you more...")
+        builder.CardAction.imBack(session, "Do you have a diagram?", "MJ can tell you more...")
     ]);
     session.endDialog(new builder.Message(session).addAttachment(card));
 }
--- dialogs/intro/chatbot-stage-help.js
+++ dialogs/intro/chatbot-stage-help.js
@@ -3,5 +3,5 @@ var builder = require('botbuilder');
 module.exports = [introYou];
 
 function introYou(session){
-    session.send("Oh cool! Maybe you should start by introducing yourselves?");
+    session.send("Oh cool! Maybe you should start by introducing yourself?");
 }
